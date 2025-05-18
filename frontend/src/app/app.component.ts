c5d89a75d459e506f8a769b4f7ca11f352e3abc5
Nesa002 <nenad.beric00@gmail.com>
2025-05-18 19:29:21 +0200

    sidebar skeleton added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/header/header.component';
 import { ModalComponent } from './auth/modal/modal.component';
 import { ToastComponent } from './shared/toast/toast/toast.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ModalComponent, ToastComponent],
+  imports: [HeaderComponent, ModalComponent, ToastComponent, SidebarComponent],
 })
 export class AppComponent {}
